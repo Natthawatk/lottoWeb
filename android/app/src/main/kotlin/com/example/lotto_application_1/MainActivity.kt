09acde4f1138f057905b62f2763388a63bcbbfbf
Natthawatk <165338045+Natthawatk@users.noreply.github.com>
2024-09-11 16:17:58 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/lotto_application_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lotto_application_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:lotto_application_1/pages/Dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lotto Web',
+      home: Dashboard(),
+    );
+  }
+}
